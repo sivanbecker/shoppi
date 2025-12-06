27bf97c8ce85423058f1adb34a12428b67b47aec
Sivan Becker <sbecker@infinidat.com>
2025-12-06 08:51:45 +0200

    add item route comments to impl

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,10 +3,16 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  // GET /items
+  // GET /items/:id
+  // POST /items
+  // PUT /items/:id
+  // DELETE /items/:id
 }
